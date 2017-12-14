fc0f6b957cf2cff40ef68e9316cc32b0aa41849f
Peter Norman <october8th@hotmail.com>
2017-12-13 23:50:40 -0500

    more firebase code

--- assets/javascript/PNCode.js
+++ assets/javascript/PNCode.js
@@ -12,18 +12,22 @@ firebase.initializeApp(config);
 // Assign the reference to the database to a variable named 'database'
 //var database = ...
 var database = firebase.database();
-
 var game = 
 {//create a game object
   	time: 0,
   	question: 0,
   	timeID: 0,
+  	player:0,
+  	categoryList: ["Science & Nature","Sports","Geography","History","Celebrities","Art"],
+  	categoryNums: [17,21,22,23,26,25],
+  	category:0,
+  	categoryName: "None",
   	player1Score:0,
   	player2Score:0,
-  	Player1Time:0,
-  	Player2Time:0,
-  	Player1Name:"None",
-  	Player2Name:"None",
+  	player1Time:0,
+  	player2Time:0,
+  	player1Name:"None",
+  	player2Name:"Not Arrived",
   	myDivGameArea: $("#content"),
   	myDivTimeRemaining:$("<div/>", {"id": "timeRemaining"}),
   	myDivQuestion:$("<div/>", {"id": "questionPic"}),
@@ -31,227 +35,248 @@ var game =
   	myDivAnswer2:$("<div/>", {"class": "answer col-12"}),
   	myDivAnswer3:$("<div/>", {"class": "answer col-12"}),
   	myDivAnswer4:$("<div/>", {"class": "answer col-12"}),
+  	questions:0,
   	startNewGame: function() 
   	{//set up the first question and initialize the div and variables
-	    game.time = 10;
 	    game.question = 0;
-	    game.player1Score =0;
-	    game.player2Score =0;
-	    game.player1Time =0;
-	    game.player2Time =0;
-	    $.ajax(
+	    game.displayQuestions();
+	},
+	getQuestions: function()
+	{
+		$.ajax(
 	    {
-			url: 'https://opentdb.com/api.php?amount=10&category=20&difficulty=medium&type=multiple',
+			url: 'https://opentdb.com/api.php?amount=10&category=' + game.category + '&difficulty=medium&type=multiple',
 			method: 'GET',
 		}).done(function(response) 
 		{
 			//console.log(response);
 			// looping through results and log
+			database.ref("Game").child("questions").remove();
 			for (var i = 0; i < response.results.length; i++) 
 			{
-				console.log(response.results[i]);
 				game.loadQuestions(response.results[i],i);
 			}
 		});
 	},
 	loadQuestions: function(object,index)//load the new questions into the database
 	{
+		var randomAnswer = Math.floor(Math.random() * 4);
+		var answers = object.incorrect_answers;
+		answers.splice(randomAnswer, 0, object.correct_answer);
+		database.ref("Game").child("questions").push(
+		{
+			questionNumber:index,
+			question: object.question,
+			theAnswer: randomAnswer,
+			theAnswers: object.incorrect_answers
+		});
+
+/*
+
 		database.ref("Game").child("question" + index).child("question").set(object.question);
-		database.ref("Game").child("question" + index).child("answer").set(object.correct_answer);
-		database.ref("Game").child("question" + index).child("wrong1").set(object.incorrect_answers[0]);
-		database.ref("Game").child("question" + index).child("wrong2").set(object.incorrect_answers[1]);
-		database.ref("Game").child("question" + index).child("wrong3").set(object.incorrect_answers[2]);
+		database.ref("Game").child("question" + index).child("theAnswer").set(randomAnswer);
+		database.ref("Game").child("question" + index).child("theAnswers").set(object.incorrect_answers);*/
+		game.shallWePlay();
 	},
-	getPlayerName: function()
-	{			            
-		$("#content").append('Enter your name to begin:<br><form name="message" action=""> Player Name: <input name="usermsg" type="text" id="userName" class="col-8"/><input name="submitName" type="submit"  id="submitName" value="Send" /></form></div></div>')
-	}
+	shallWePlay: function()//waiting for player 2 or just start the game
+	{
+		game.myDivGameArea.empty();//clear out my div and add the category buttons
+		game.myDivGameArea.append("<div><h4 class='text-center' id = 'prompt'>The Category is: " + game.categoryName +" </h4></div>");
+		game.myDivGameArea.append("<div><h5 class='text-center' id = 'player1Name'>Player 1: " + game.player1Name  +" </h5></div>");
+		game.myDivGameArea.append("<div><h5 class='text-center' id = 'player2Name'>Player 2: " + game.player2Name +" </h5></div>");
+		var myButton = $("<button/>", {"id": "startTheGame"});
+		myButton.text("Start")
+		game.myDivGameArea.append(myButton);
+	},
+	getPlayerName: function()//prompt for the player name
+	{			     
+		game.myDivGameArea.empty();       
+		game.myDivGameArea.append('Enter your name to begin:<br><form name="message" action=""> Player Name: <input name="usermsg" type="text" id="userName" class="col-8"/><input name="submitName" type="submit"  id="submitName" value="Send" /></form></div></div>')
+	},
+	displayCategories: function()
+	{
+		game.myDivGameArea.empty();//clear out my div and add the category buttons
+		game.myDivGameArea.append("<div><h4 class='text-center' id = 'prompt'>Please choose a category:</h4></div>");
+		$(game.categoryList).each(function(index,item)
+	    {
+	    	//display the categories
+	    	var newDiv = $("<button/>", {"class": "categoryChoice col-12"});
+	    	newDiv.attr("data-c",game.categoryNums[index]);
+	    	newDiv.attr("data-n",item);
+		    game.myDivGameArea.append(newDiv.text(item));
+	    });
+	},
+	displayQuestions: function()
+	{
+		//alert(snapshot.child("Game").child("question0").child("wrong4").val()[2]);
+		database.ref("Game").child('questions').orderByChild('questionNumber').equalTo(5).on("value", function(snapshot) {
+    		console.log(snapshot.val());
+    		console.log(snapshot.child("question").val());
+    		snapshot.forEach(function(data) {
+        				console.log(data.key);
+    				});
+    		//console.log(snapshot.child("question").val());
+		});
+		//console.log(game.questions[0]);
+		game.myDivGameArea.empty();//clear out my div and add the questions
+		game.myDivGameArea.append("<div><h4 class='text-center' id = 'timeRemaining'>Time:</h4></div>");
+		game.myDivGameArea.append("<div><h4 class='text-center' id = 'prompt'>" + game.questions[0]+"</h4></div>");
+		/*$(game.categoryList).each(function(index,item)
+	    {
+	    	//display the categories
+	    	var newDiv = $("<button/>", {"class": "categoryChoice col-12"});
+	    	newDiv.attr("data-c",game.categoryNums[index]);
+	    	newDiv.attr("data-n",item);
+		    game.myDivGameArea.append(newDiv.text(item));
+	    });*/
+	},
+	outtaTime: function() 
+	{/*//the timer hit zero
+	  	if(game.question < (questions.length -1))
+	  	{
+	  		clearInterval(game.timeID);
+	  		$("#timeRemaining").text("Time Remaining: 0");
+	  		$("#prompt").text("You're outta time! it's: " + pokemon[game.question] + "!");
+	  		$(game.myDivQuestion).html('<img src= '+ questions[game.question].apic +' class ="question rounded mx-auto d-block img-fluid"/>');
+	  		game.clearDiv();
+	  		setTimeout(function(){ game.newQuestion(); }, 4000);
+	  	}
+	  	else
+	  	{
+	  		clearInterval(game.timeID);
+	  		$("#timeRemaining").text("Time Remaining: 0");
+	  		$("#prompt").text("You're outta time! it's: " + pokemon[game.question] + "!");
+	  		$(game.myDivQuestion).html('<img src= '+ questions[game.question].apic +' class ="question rounded mx-auto d-block img-fluid"/>');
+	  		game.clearDiv();
+	  		setTimeout(function(){ game.gameOver(); }, 4000);
+	  	}*/
+  	},
+	count: function(){//count down from a given number of seconds
+  	if(game.time > 0)
+  	{
+
+  		database.ref("Game").child("timer").set(game.time);//set the timer to 15 seconds
+  		game.time --
+  	}
+  	else
+  	{
+  		game.outtaTime();
+  	}
+  },
 
 };
-
-game.getPlayerName();
-game.startNewGame();
 
 $(document).on("click", "#submitName" , function(event)//enter your name
 {	// Prevent form from submitting
   	event.preventDefault();
-  // Get the input values
+  // Get the input value and send it to the database
   var playerName = $("#userName").val().trim();
-  database.ref("Game").child("Player").set(playerName);
-  $('#userName').val('');
+  if(playerName.length > 0)
+  {
+  	database.ref("Game").child("player" + game.player + "Name").set(playerName);
+  	if(game.player == 1)
+  	{
+  		game.player1Name = playerName;
+  		game.displayCategories();
+  	}
+  	else if(game.player == 2)
+  	{
+  		game.player2Name = playerName;
+  	}
+  	$('#userName').val('');
+  }
 });
 
-/*			
-	    $(pokemon).each(function(index,item)
-	    {
-	    	// create the questions
-	    	questions[index] = new questionObj(index,item);
-	    });
-	game.myDivGameArea.empty();
-    game.myDivGameArea.append(game.myDivTimeRemaining);
-    game.myDivGameArea.append("<div><h4 class='text-center' id = 'prompt'>Can you guess the Pokemon?</h4></div>");
-    game.myDivGameArea.append(game.myDivQuestion.html('<img src= '+ questions[game.question].qpic +' class ="question rounded mx-auto d-block img-fluid"/>'));
-    game.myDivGameArea.append(game.myDivAnswer1.html(questions[game.question].answers[0]));
-    game.myDivAnswer1.attr("data-a",0);
-    game.myDivGameArea.append(game.myDivAnswer2.html(questions[game.question].answers[1]));
-    game.myDivAnswer2.attr("data-a",1);
-    game.myDivGameArea.append(game.myDivAnswer3.html(questions[game.question].answers[2]));
-    game.myDivAnswer3.attr("data-a",2);
-    game.myDivGameArea.append(game.myDivAnswer4.html(questions[game.question].answers[3]));
-    game.myDivAnswer4.attr("data-a",3);
-    $(game.myDivAnswer1).addClass("answer");
-  	$(game.myDivAnswer2).addClass("answer");
-  	$(game.myDivAnswer3).addClass("answer");
-  	$(game.myDivAnswer4).addClass("answer");
-
- // The page has loaded for the first time
-$( document ).ready(function() {
-	getCurrentGameInfo();
-
-    getPlayerName();
-    //initializeGame();
+$(document).on("click", ".categoryChoice" , function(event)//enter your name
+{
+  // Get the category choice and get the questions
+  // then we wait for player 2 or start the game
+  game.category = $(this).data("c");
+  game.categoryName = $(this).data("n");
+  database.ref("Game").child("Category").set(game.categoryName);
+  game.getQuestions();
 });
 
-//welcome player enter your name
-function getPlayerName(){
-
-}
-
-  $(document).ready(function() {
-
-	$.ajax({
-		url: 'https://opentdb.com/api.php?amount=10',
-		method: 'GET',
-	}).done(function(response) {
-		console.log(response);
-
-		// looping over results, and printing each question
-		for (var i = 0; i < response.results.length; i++) {
-			//if (i === 0) printQuestion(response.results[i]);
-			printQuestion(response.results[i]);
-
-		}
-	});
-
-	// expects object:
-	// 	{
-	//		question: 'string',
-	//		incorrect_answers: [],
-	//		correct_answer: 'string',
-	// 	}
-	//
-	// 	returns a div with children:
-	//		question,
-	//		list of answers
-	function printQuestion(question) {
-		var question_container = $('<div>');
-
-		var question_element = $('<h1>').html(question.question);
-
-		// create answers array from correct 
-		// answer and incorrect answers
-		var answers = [];		
-		question.incorrect_answers.forEach(function (answer) {
-			answers.push({ text: answer, data: 'incorrect' });
-		});
-		answers.push({ 
-			text: question.correct_answer, 
-			data: 'correct',
-		});
-
-		// passing our array and an object with possible on 
-		// click event to bind on each child
-		var answers_element = createElementsFromArray(answers, {
-			clickEvent: function(event) {
-				if (this.dataset.correct) {
-					$(this).addClass('correct');
-				} else if (this.dataset.incorrect) {
-					$(this).addClass('incorrect');
-				}
-			},
-		});
-
-		// append our new elements to our parent
-		question_container.append(question_element);
-		question_container.append(answers_element);
-		
-		// append to the page
-		$('#app').append(question_container);
-	}
-
-	// expects:
-	// [
-	//		{ text: 'string', data: 'string' }
-	// ],
-	// optional {
-	//		clickEvent: function() {}
-	// }
-	function createElementsFromArray(array, options) {
-
-		// create parent element
-		var elementsList = $('<ul>');
-
-		// loop to create children
-		for (var i = 0; i < array.length; i++) {
-
-			// create child element with text from object
-			var child = $('<li>').html(array[i].text);
-			child.attr('data-' + array[i].data, 'true');
-
-			// if there is a clickEvent, add it to each child
-			if (options && options.clickEvent) {
-				child.on('click', options.clickEvent);
-			}
-
-			// add child to parent
-			elementsList.append(child);
-		}
-
-		return elementsList;
-
-	}
-
+$(document).on("click", "#startTheGame" , function(event)//start the game
+{	//create the gameplay div, load question 1 and start the timer
+	
+  	game.time = 15;
+    game.timeID = setInterval(function(){ game.count(); }, 1000); 
+  	game.startNewGame();
 });
 
 
-/*pseudocode section for game section
 
-function initialize game{
-	pick player number 1 or 2
-	choosse 1 or 2 player game
-	choose a category
-}
+database.ref("Game/timer").on("value", function(snapshot) //the timer is counting down
+{
+	game.time = snapshot.child("Game").child("timer").val();
+	$("#timeRemaining").text("Time Remaining: " + game.time);
+});
 
-function get questions{
-	grab the 10 questions and save them in the database
-}
+database.ref().on("value", function(snapshot) //any change on the database triggers this
+{
+	//listen for changes to the database
+	//alert(snapshot.child("Game").child("question0").child("wrong4").val()[2]);
+	//game.questions = snapshot.child("Game").child("questions").val();
+	//console.log(game.questions);
+	if(game.player == 0)//this is our first look at the database
+	{
+		//Can I be player 1?
+  		if(snapshot.child("Game").child("player1Here").val() == null)
+  		{
+  			//I can be player1
+ 			game.player = 1;
+  			database.ref("Game").child("player1Here").set("yes");
+  			if(snapshot.child("Game").child("player2Here").val() == null)
+  			{
+  				//there is no player 2
+  				game.player2Name = "Not arrived";
+  			}
+  			else//thre is a player 2 - lets grab the name
+  			{
+  				game.player2Name = snapshot.child("Game").child("player2Name").val();
+  			}
+  			game.getPlayerName();
+  		}//i can't be player 1, can I be player 2?
+  		else if(snapshot.child("Game").child("player2Here").val() == null)
+  		{
+  			//I can be player 2
+  			game.player = 2;
+  			database.ref("Game").child("player2Here").set("yes");
+  			if(snapshot.child("Game").child("player1Here").val() == null)
+  			{
+  				//there is no player 1
+  				game.player1Name = "Not arrived";
+  			}
+  			else//thre is a player 1 - lets grab the name
+  			{
+  				game.player2Name = snapshot.child("Game").child("player1Name").val();
+  			}
+  			game.getPlayerName();
+  		}
+  		else
+  		{
+  			game.myDivGameArea.empty();//clear out my div and add the category buttons
+			game.myDivGameArea.append("<div><h4 class='text-center' id = 'prompt'>The Game is already full.  Please come back later.</h4></div>");
+  		}
+  	}
+}, function(errorObject) {
+  console.log("The read failed: " + errorObject.code);
+});
 
-function ask a question{
-	display question
-	display choices
-}
+window.addEventListener("beforeunload", function (e) {
+  var confirmationMessage = "\o/";
 
-function countdown{
-	count down timer 20 secs / question
-}
-
-function process answer choices{
-	get the player(s) answers and send to database if needed
-	check them against the right answer
-	update scores to database
-}
-
-function show corect answer{
-	show answer with a gif from gify
-	update scores to local screens move on to next question or end game if q 10
-}
-
-function end game{
-	show final score
-	offer another round or just start next round.
-}
-
-
-
-
-*/
+  (e || window.event).returnValue = confirmationMessage; //Gecko + IE
+  if(game.player == 1)//remove player 1
+  {
+  	database.ref("Game").child("player1Here").remove();
+  	database.ref("Game").child("player1Name").remove();
+  }
+  else if(game.player == 2)//remove player 2
+  {
+  	database.ref("Game").child("player2Here").remove();
+  	database.ref("Game").child("player2Name").remove();
+  }
+  return confirmationMessage;   //Webkit, Safari, Chrome
+});
