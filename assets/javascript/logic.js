6516e924a4ca49212b37203043c96ecd3087a3b4
unknown <you@example.com>
2017-12-21 02:44:19 -0500

    fixed token error

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -64,7 +64,6 @@ var game =
   {
     if(game.question == 9)//the game is over
     {
-      game.showGameResults();
       game.started = 0;
       database.ref("Game").child('Started').set(0);//the game is starting
       game.go = 0;
@@ -78,36 +77,25 @@ var game =
         else//you're player 2 
         {
           database.ref("Player1").remove();
-
-          //This Code is to push plater 2 stats
-          var userName2 = game.playerName[1];
-          var userScore2 = game.playerScore[1];
-          var userTime2 = game.playerTime[1];
-
-          var newUser2 = {
-            name: userName2,
-            score: userScore2,
-            time: userTime2,
-            }
-          database.ref("/Highscores").push(newUser2);
         }
 
-          //pushes player1 stats
-        }
+          var userName = "lary";
+          var userScore = 50;
+          var userTime = "00:00";
 
-          var userName1 = game.playerName[0];
-          var userScore1 = game.playerScore[0];
-          var userTime1 = game.playerTime[0];
-
-          var newUser1 = {
-            name: userName1,
-            score: userScore1,
-            time: userTime1,
+          var newUser = {
+            name: userName,
+            score: userScore,
+            time: userTime,
             }
 
-          database.ref("/Highscores").push(newUser1);
+          database.ref("/Highscores").push(newUser);
+          console.log("The game has ended");
+          console.log(newUser);
+          console.log(userScore);
         }
       clearInterval(game.timeID)//
+      game.shallWePlay();
     }
     else
     {
@@ -335,46 +323,55 @@ var game =
   updateScoreboard: function()
   {
     var currentQuestion = game.question + 1;
+    if(currentQuestion == 10)//make the font bold somehow?
+    {
+      $('#scoreboard-table > tbody:last-child').prepend('<tr class="animated fadeIn"><td>' + currentQuestion + '</td><td>' + game.playerScore[0] + '</td><td>' + game.playerTime[0] + '</td><td>' + game.playerScore[1] + '</td><td>' + game.playerTime[1] + '</td></tr>');
+    }
+    else
+    {
+      $('#scoreboard-table > tbody:last-child').prepend('<tr class="animated fadeIn"><td>' + currentQuestion + '</td><td>' + game.playerScore[0] + '</td><td>' + game.playerTime[0] + '</td><td>' + game.playerScore[1] + '</td><td>' + game.playerTime[1] + '</td></tr>');
+    }
+    
+    
+  },
+  leaderboardEndGame: function()
+{//adds to the database
+//user input is put into variables
+  var userName = $("#player").val().trim();
+  var userScore = game.playerScore;
+  var userTime = game.playerTime;
 
-    $('#scoreboard-table > tbody:last-child').prepend('<tr class="animated fadeIn"><td>' + currentQuestion + '</td><td>' + game.playerScore[0] + '</td><td>' + game.playerTime[0] + '</td><td>' + game.playerScore[1] + '</td><td>' + game.playerTime[1] + '</td></tr>');
-
-   },
-  showGameResults: function()
-  {
-    game.myDivGameArea.empty();
-    var gameResultsDiv = $('<div class="gameResultsDiv">');
-      if (game.playerScore[0] > game.playerScore[1]) //if player 1 has higher score
-      {  
-      gameResultsDiv.html(game.playerName[0] + ' is smarter than ' + game.playerName[1] + '!').addClass('player1FinalStyle');
-      } 
-      else if (game.playerScore[0] < game.playerScore[1]) //if player 2 has higher score
-      { 
-      gameResultsDiv.html(game.playerName[1] + ' is smarter than ' + game.playerName[0] + '!').addClass('player2FinalStyle');
-      } 
-      else if (game.playerScore[0] == game.playerScore[1] && game.playerTime[0] > game.playerTime[1]) //if tie, but player 1 was faster
-      {  
-      gameResultsDiv.html(game.playerName[0] + ' may not be smarter, but is definitely faster than ' + game.playerName[1] + '!').addClass('player1FinalStyle');
-      } 
-      else if (game.playerScore[0] == game.playerScore[1] && game.playerTime[0] < game.playerTime[1]) //if tie, but player 2 was faster
-      {  
-      gameResultsDiv.html(game.playerName[1] + ' may not be smarter, but is definitely faster than ' + game.playerName[0]).addClass('player2FinalStyle');
-      }
-    var playAgainDiv = $('<div class="playAgainDiv">');
-    playAgainDiv.html('Play again?');
-    gameResultsDiv.append(playAgainDiv);
-    game.myDivGameArea.append(gameResultsDiv);
-    var myButton = $("<button class='animated infinite pulse' id='startTheGame'>");
-    myButton.text("Start");
-    playAgainDiv.append(myButton);
+  var newUser = {
+    name: userName,
+    score: userScore,
+    time: userTime,
   }
+
+  database.ref().push(newUser);
+
+},
+
 };
 
 $(document).ready(function() 
 {//when the document loads the first time
   //hide chat log on page load
   $('#chat').hide();
-  $('#scoreboard-panel').hide();
+
+  //shows the scores
+  database.ref("/Highscores").on("value", function(snapshot) {
+
+  console.log(snapshot.val());
+
+ $.each(snapshot.val(), function(k, v)
+  {
+    console.log(k)
+    console.log(v)
+
+    $("#achievements").append("<div><strong>" + v.name + "</strong><em> Score: </em>: " + v.score + "<em> Time: </em>" + v.time + "</div>");
+    });
   });
+});
 
 $(document).on("click", "#name-btn" , function(event)//enter your name
 { // Prevent form from submitting
@@ -393,7 +390,6 @@ $(document).on("click", "#name-btn" , function(event)//enter your name
 
 $(document).on("click", ".categoryChoice" , function(event)//enter your name
 {
-  $('#scoreboard-panel').show();
   // Get the category choice and get the questions
   // then we wait for player 2 or start the game
   game.category = $(this).data("c");
@@ -439,7 +435,6 @@ $(document).on("click", "#startTheGame" , function(event)//start the game
       }
     }
     game.started = 1;
-    $('#scoreboard-table > tbody').empty();
     database.ref("Game").child('Started').set(1);//the game is starting
     game.displayCategories();
 });
@@ -477,15 +472,15 @@ database.ref("Player1/Answered").on("value", function(snapshot) //the timer is c
       //show the correct answer page
       //game.displayAnswer();
       //kill the timer
-      //update the scoreboard-table
+      //update the scorboard-table
       setTimeout(function(){ game.updateScoreboard(); }, 500);
       clearInterval(game.timeID);
       setTimeout(function(){ game.nextQuestion(); }, 8000);
     }
     else if((game.playingComputer == 1)&& (snapshot.val() == 1))
     {
-      //update the scoreboard-table
-      //update the scoreboard-table
+      //update the scorboard-table
+      //update the scorboard-table
       setTimeout(function(){ game.updateScoreboard(); }, 500);
       clearInterval(game.timeID);
       setTimeout(function(){ game.nextQuestion(); }, 8000);
@@ -721,19 +716,3 @@ $('#chat-btn').on('click', function() {
 });
 
 showChats();
-
-//This shows the leaderboard when the window loads
-window.onload =function(){
-//when the document loads the first time
-  //shows the scores
-
-  database.ref("/Highscores").on("value", function(snapshot) {
-
-  console.log(snapshot.val());
-
- $.each(snapshot.val(), function(k, v)
-  {
-    $("#achievements").append("<div><strong>" + v.name + "</strong><em> Score: </em>: " + v.score + "<em> Time: </em>" + v.time + "s</div>");
-    });
-  });
-};
